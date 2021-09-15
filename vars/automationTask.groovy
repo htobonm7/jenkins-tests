7518ac9e4bd4b9726e63a66607dbad423f3934e7
Hamilton Tobon Mosquera <hamilton.tobonm@perficient.com>
2021-09-15 16:26:46 -0500

    Shared library

--- vars/automationTask.groovy
+++ vars/automationTask.groovy
@@ -1,4 +1,7 @@
 def call(Closure body) {
+  properties([
+    parameters([string(defaultValue: 'Hello', description: 'How should I greet the world?', name: 'Greeting')])
+  ])
   stage('Step 1') {
     echo 'Running an automation task.'
     body()
